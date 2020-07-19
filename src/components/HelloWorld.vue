f775eb95e9d522720a070105bd21ae41c72c2fa2
r-tomiyama <r.tomiyama@opt.ne.jp>
2020-07-19 22:26:52 +0900

    install composition-api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,135 +9,76 @@
           height="200"
         />
       </v-col>
+    </v-row>
 
+    <v-row class="text-center">
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Poyo</h1>
+        <p>count: {{ state.count }}, double: {{ state.double }}</p>
+        <v-btn class="mr-4" @click="increment">ぽよ</v-btn>
       </v-col>
+    </v-row>
 
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <v-form>
+          <v-text-field
+            v-model="poyo"
+            :counter="100"
+            label="ぽよ"
+          ></v-text-field>
+          <p>{{ state.hoge }}</p>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+          <v-btn color="error" class="mr-4" @click="reset">リセット</v-btn>
+        </v-form>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { computed, defineComponent, reactive } from "@vue/composition-api";
 
-export default Vue.extend({
+export default defineComponent({
   name: "HelloWorld",
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+  props: {
+    poyo: {
+      type: String,
+      default: ""
+    }
+  },
+
+  setup(props: Props) {
+    const state: State = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+      hoge: computed(() => "『" + props.poyo + "』")
+    });
+
+    function increment() {
+      state.count++;
+    }
+    function reset() {
+      state.count = 0;
+      props.poyo = "";
+    }
+
+    return {
+      state,
+      increment,
+      reset
+    };
+  }
 });
+
+type Props = {
+  poyo: string;
+};
+
+interface State {
+  count: number;
+  double: number;
+  hoge: string;
+}
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueCompositionApi from "@vue/composition-api";
 import vuetify from "./plugins/vuetify";
+
+Vue.use(VueCompositionApi);
 
 Vue.config.productionTip = false;
 
